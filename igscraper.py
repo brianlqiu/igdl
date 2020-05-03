3a47fa8ed66a34ac709c605f162935130ccf01da
Brian Qiu <brianlqiu5@gmail.com>
2020-05-03 01:44:22 -0700

    added support for carousel pictures

--- igscraper.py
+++ igscraper.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from bs4 import BeautifulSoup
@@ -11,59 +12,72 @@ def is_photo_link(tag):
 
 def get_content(tag):
     carousel = False
+    image = False
     if tag.name == 'img' and tag.parent.has_attr('class'):
         for class_item in tag.parent['class']:
             if class_item == 'carousel-item':
                 carousel = True
-    image = False
-    if tag.name == 'img' and tag.parent.has_attr('class'):
-        for class_item in tag.parent['class']:
             if class_item == 'all-m':
                 image = True
     video = tag.name == 'source' and tag.parent.name == 'video'
     return carousel or image or video
 
+#Settings
 user = 'theweeknd'
 num_posts = 10
+scroll = False
 
+#Setup
 print('Booting up...')
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
-driver = webdriver.Chrome('/mnt/c/Users/brian/chromedriver_win32/chromedriver.exe', options=options)
+driver = webdriver.Chrome('/mnt/c/Users/brian/chromedriver_win32/chromedriver.exe')
 driver.get('https://imgtagram.com/u/' + user)
-last_height = 0
-"""
-for i in range(1,100):
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(1.5)
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        break
-    last_height = new_height
-"""
+
+#Scroll
+if scroll:
+    last_height = 0
+    for i in range(1,100):
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(1.5)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
 html_source = driver.page_source
-soup = BeautifulSoup(html_source, 'html.parser')
+
+#Getting anchor tags
 print("Getting anchor tags...")
+soup = BeautifulSoup(html_source, 'html.parser')
 links = soup.find_all(is_photo_link)
 print("Finished getting anchor tags.")
+
+#Getting post links
+print("Getting post links from anchor tags...")
 hrefs = []
-print("Getting links from anchor tags...")
 for link in links:
     print("Got " + link.get('href'))
     hrefs.append(link.get('href'))
 print("Finished getting links.")
+
+#Getting post source code
+print("Getting posts sources...")
 post_sources = []
-i = 0
-print("Getting posts...")
 for href in hrefs:
     print("Getting post from " + href)
-    if i > num_posts:
-        break
     driver.get('https://imgtagram.com' + href)
     WebDriverWait(driver, 10).until(lambda driver: driver.execute_script('return document.readyState') == 'complete')
-    post_sources.append(driver.page_source)
-    i = i + 1
+    try:
+        carousel = driver.find_element_by_class_name('carousel-control-next')
+        ActionChains(driver).click(carousel).perform()
+        time.sleep(0.5)
+    except:
+        print("Not a carousel.")
+    finally:
+        post_sources.append(driver.page_source)
 print("Finished getting posts.")
+
+#Getting content from the source code
 print("Getting content...")
 content = []
 for post_source in post_sources:
@@ -71,7 +85,9 @@ for post_source in post_sources:
     content_tags = soup.find_all(get_content)
     for content_tag in content_tags:
         content.append(content_tag.get('src'))
+driver.close()
+print("Done")
+
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(content)
     
-driver.close()
